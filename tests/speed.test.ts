d6fd1ffc2611749bdb4eb01115d64413fb868fd3
Speykious <speykious.the.king@gmail.com>
2020-08-10 20:03:47 +0200

    Abstracted and reinforced speed tests

--- tests/speed.test.ts
+++ tests/speed.test.ts
@@ -6,61 +6,42 @@ import { format } from "util"
 
 const utf8_txt = resolve(__dirname, "./utf8.txt")
 const dump_txt = resolve(__dirname, "./dump.txt")
-const samples = 100
+const bible_txt = resolve(__dirname, "./bible.txt")
 const orange = rgb24(0xffaa64)
 const lightb = rgb24(0x64aaff)
 
-
-test(`Speed difference on ${samples} loop samples | small file`, async () => {
+const abstract = async (detail: string, file: string, samples: number, lines: number) => {
 	const start_rll = new Date()
 	for (let i = 0; i < samples; i++)
-		await rllo(utf8_txt, 10)
+		await rllo(file, lines)
 	const rll_ms = new Date().getTime() - start_rll.getTime()
 
 	const start_rllts = new Date()
 	for (let i = 0; i < samples; i++)
-		rll(utf8_txt, 10)
+		rll(file, lines)
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
 
 	expect(rllts_ms * 5).toBeLessThan(rll_ms)
 																													// Nested interpolation <_<
 console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
 Time taken (${lightb("read-last-lines-ts")}): ${lightb(`${rllts_ms} ms`)}
-rllts has performed ${bold((rll_ms / rllts_ms).toFixed(2))} times faster than rll ${dim("(small file, all lines)")}`)
-})
+rllts has performed ${bold((rll_ms / rllts_ms).toFixed(2))} times faster than rll ${dim(`(${detail})`)}`)
+}
 
-test(`Speed difference on ${samples} loop samples | big file`, async () => {
-	const start_rll = new Date()
-	for (let i = 0; i < samples; i++)
-		await rllo(dump_txt, 10)
-	const rll_ms = new Date().getTime() - start_rll.getTime()
 
-	const start_rllts = new Date()
-	for (let i = 0; i < samples; i++)
-		rll(dump_txt, 10)
-	const rllts_ms = new Date().getTime() - start_rllts.getTime()
+test(`Speed difference | small file`, () =>
+	abstract("small file, all lines", utf8_txt, 100, 10))
 
-	expect(rllts_ms * 3).toBeLessThan(rll_ms)
+test(`Speed difference | medium file, few lines`,
+	() => abstract("medium file, few lines", dump_txt, 100, 10),
+	10000)
+test(`Speed difference | medium file, all lines`,
+	() => abstract("medium file, all lines", dump_txt, 10, 300),
+	10000)
 
-console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
-Time taken (${lightb("read-last-lines-ts")}): ${lightb(`${rllts_ms} ms`)}
-rllts has performed ${bold((rll_ms / rllts_ms).toFixed(2))} times faster than rll ${dim("(big file, few lines)")}`)
-})
-
-test(`Speed difference on ${samples / 10} loop big samples | big file`, async () => {
-	const start_rll = new Date()
-	for (let i = 0; i < samples / 10; i++)
-		await rllo(dump_txt, 300)
-	const rll_ms = new Date().getTime() - start_rll.getTime()
-
-	const start_rllts = new Date()
-	for (let i = 0; i < samples / 10; i++)
-		rll(dump_txt, 300)
-	const rllts_ms = new Date().getTime() - start_rllts.getTime()
-
-	expect(rllts_ms * 3).toBeLessThan(rll_ms)
-
-console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
-Time taken (${lightb("read-last-lines-ts")}): ${lightb(`${rllts_ms} ms`)}
-rllts has performed ${bold((rll_ms / rllts_ms).toFixed(2))} times faster than rll ${dim("(big file, all lines)")}`)
-})
+test(`Speed difference | large file, few lines`,
+	() => abstract("large file, few lines", bible_txt, 100, 10),
+	10000)
+test(`Speed difference | large file, lots of lines`,
+	() => abstract("large file, lots of lines", bible_txt, 10, 300),
+	10000)
