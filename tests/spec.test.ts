52c352f718398b379fc2900f31c0a6c5e0b9a0f0
Speykious <speykious.the.king@gmail.com>
2020-08-10 04:40:07 +0200

    Finished writing tests for now

--- tests/spec.test.ts
+++ tests/spec.test.ts
@@ -1,35 +1,44 @@
-import { readLastLinesEnc as rlle, readLastLines as rll } from "../lib"
+import { readLastLinesEnc as rlle, readLastLines as rll } from "../lib/index"
+import { resolve } from "path"
+
+const utf8_txt               = resolve(__dirname, "./utf8.txt")
+const numbered_txt           = resolve(__dirname, "./numbered.txt")
+const numbered_ntnl_txt      = resolve(__dirname, "./numbered-ntnl.txt")
+const i_dont_exist_sorry_lol = resolve(__dirname, "./i-dont-exist-sorry-lol")
 
 test("Get all lines when asked for more than the file has", () => {
-	const allLines = rlle("utf8")("tests/numbered.txt", 20)
+	const allLines = rlle("utf8")(numbered_txt, 20)
 	expect(allLines.split(/\n/).length).toBe(10 + 1)
 })
 
 test("Get last line when asked for 1", () => {
-	const lastLine = rlle("utf8")("tests/numbered.txt", 1)
+	const lastLine = rlle("utf8")(numbered_txt, 1)
 	expect(lastLine.split(/\n/).length).toBe(1 + 1)
 	expect(lastLine.trim().length).toBe(3)
 })
 
 test("Get last line when asked for 2", () => {
-	const lastLine = rlle("utf8")("tests/numbered.txt", 2)
+	const lastLine = rlle("utf8")(numbered_txt, 2)
 	expect(lastLine.split(/\n/).length).toBe(2 + 1)
 	expect(lastLine.trim().length).toBe(2 * 3 + 1)
 })
 
 test("Get last line when asked for 2 (no trailing newline)", () => {
-	const lastLine = rlle("utf8")("tests/numbered-ntnl.txt", 2)
+	const lastLine = rlle("utf8")(numbered_ntnl_txt, 2)
 	expect(lastLine.split(/\n/).length).toBe(2)
 	expect(lastLine.trim().length).toBe(2 * 3 + 1)
 })
 
 test("File's existential crisis", () => {
-	expect(rll("tests/i-dont-exist-sorry-lol", 1))
-	.toThrowError("File 'tests/i-dont-exist-sorry-lol' doesn't exist :(")
+	try {
+		rll(i_dont_exist_sorry_lol, 1)
+	} catch (err) {
+		expect(err).toEqual(new Error(`File '${i_dont_exist_sorry_lol}' doesn't exist :(`))
+	}
 })
 
 test("Reading UTF8 files", () => {
-	const lines = rlle("utf8")("tests/utf8.txt", 2)
+	const lines = rlle("utf8")(utf8_txt, 2)
 	expect(lines).toMatch("english")
 	expect(lines).toMatch("中文")
 	expect(lines).toMatch("español")
--- tests/speed.test.ts
+++ tests/speed.test.ts
@@ -1,32 +1,38 @@
-import { readLastLinesEnc as rlle, readLastLines as rll } from "../lib"
+import { readLastLinesEnc as rlle, readLastLines as rll } from "../lib/index"
 import { read as rllo } from "read-last-lines"
+import { resolve } from "path"
 
-test("Speed difference on 1000 loop samples | small file", () => {
+const utf8_txt = resolve(__dirname, "./utf8.txt")
+const dump_txt = resolve(__dirname, "./dump.txt")
+const samples = 100
+
+
+test(`Speed difference on ${samples} loop samples | small file`, async () => {
 	const start_rll = new Date()
-	for (let i = 0; i < 1000; i++)
-		rllo("../tests/utf8.txt", 10)
+	for (let i = 0; i < samples; i++)
+		await rllo(utf8_txt, 10)
 	const rll_ms = new Date().getTime() - start_rll.getTime()
 	console.log(`Time taken    (read-last-lines):`, rll_ms, "ms")
 
 	const start_rllts = new Date()
-	for (let i = 0; i < 1000; i++)
-		rll("../tests/utf8.txt", 10)
+	for (let i = 0; i < samples; i++)
+		rll(utf8_txt, 10)
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
 	console.log(`Time taken (read-last-lines-ts):`, rllts_ms, "ms")
 
-	expect(rllts_ms * 3).toBeLessThan(rll_ms)
+	expect(rllts_ms * 5).toBeLessThan(rll_ms)
 })
 
-test("Speed difference on 1000 loop samples | big file", () => {
+test("Speed difference on samples loop samples | big file", async () => {
 	const start_rll = new Date()
-	for (let i = 0; i < 1000; i++)
-		rllo("../tests/dump.txt", 10)
+	for (let i = 0; i < samples; i++)
+		await rllo(dump_txt, 10)
 	const rll_ms = new Date().getTime() - start_rll.getTime()
 	console.log(`Time taken    (read-last-lines):`, rll_ms, "ms")
 
 	const start_rllts = new Date()
-	for (let i = 0; i < 1000; i++)
-		rll("../tests/dump.txt", 10)
+	for (let i = 0; i < samples; i++)
+		rll(dump_txt, 10)
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
 	console.log(`Time taken (read-last-lines-ts):`, rllts_ms, "ms")
 
