bf843fcb099624dae1bac0b398b8592d290f7185
Speykious <speykious.the.king@gmail.com>
2020-08-10 18:35:45 +0200

    Getting console logs together for Jest

--- tests/speed.test.ts
+++ tests/speed.test.ts
@@ -1,4 +1,4 @@
-import { readLastLinesEnc as rlle, readLastLines as rll } from "../lib/index"
+import { readLastLines as rll } from "../lib/index"
 import { read as rllo } from "read-last-lines"
 import { resolve } from "path"
 
@@ -12,17 +12,17 @@ test(`Speed difference on ${samples} loop samples | small file`, async () => {
 	for (let i = 0; i < samples; i++)
 		await rllo(utf8_txt, 10)
 	const rll_ms = new Date().getTime() - start_rll.getTime()
-	console.log(`Time taken    (read-last-lines):`, rll_ms, "ms")
 
 	const start_rllts = new Date()
 	for (let i = 0; i < samples; i++)
 		rll(utf8_txt, 10)
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
-	console.log(`Time taken (read-last-lines-ts):`, rllts_ms, "ms")
 
 	expect(rllts_ms * 5).toBeLessThan(rll_ms)
 
-	console.log(`rllts has performed ${rll_ms / rllts_ms} times faster than rll`)
+	console.log(`Time taken    (read-last-lines): ${rll_ms} ms
+	Time taken (read-last-lines-ts): ${rllts_ms} ms\n
+	rllts has performed ${rll_ms / rllts_ms} times faster than rll (small file)`)
 })
 
 test("Speed difference on samples loop samples | big file", async () => {
@@ -30,15 +30,15 @@ test("Speed difference on samples loop samples | big file", async () => {
 	for (let i = 0; i < samples; i++)
 		await rllo(dump_txt, 10)
 	const rll_ms = new Date().getTime() - start_rll.getTime()
-	console.log(`Time taken    (read-last-lines):`, rll_ms, "ms")
 
 	const start_rllts = new Date()
 	for (let i = 0; i < samples; i++)
 		rll(dump_txt, 10)
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
-	console.log(`Time taken (read-last-lines-ts):`, rllts_ms, "ms")
 
 	expect(rllts_ms * 3).toBeLessThan(rll_ms)
 
-	console.log(`rllts has performed ${rll_ms / rllts_ms} times faster than rll`)
+	console.log(`Time taken    (read-last-lines): ${rll_ms} ms
+	Time taken (read-last-lines-ts): ${rllts_ms} ms\n
+	rllts has performed ${rll_ms / rllts_ms} times faster than rll (big file)`)
 })
