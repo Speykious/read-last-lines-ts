6d4a5eb036a4c9a2767e76cb3f8e18248e57e9b4
Speykious <speykious.the.king@gmail.com>
2020-08-10 18:50:57 +0200

    Nested interpolation <_<

--- tests/speed.test.ts
+++ tests/speed.test.ts
@@ -1,10 +1,13 @@
 import { readLastLines as rll } from "../lib/index"
 import { read as rllo } from "read-last-lines"
 import { resolve } from "path"
+import { dim, bold, rgb24 } from "ansi-colors-ts"
 
 const utf8_txt = resolve(__dirname, "./utf8.txt")
 const dump_txt = resolve(__dirname, "./dump.txt")
 const samples = 100
+const orange = rgb24(0xff8032)
+const lightb = rgb24(0x3280ff)
 
 
 test(`Speed difference on ${samples} loop samples | small file`, async () => {
@@ -19,10 +22,10 @@ test(`Speed difference on ${samples} loop samples | small file`, async () => {
 	const rllts_ms = new Date().getTime() - start_rllts.getTime()
 
 	expect(rllts_ms * 5).toBeLessThan(rll_ms)
-
-console.log(`Time taken    (read-last-lines): ${rll_ms} ms
-Time taken (read-last-lines-ts): ${rllts_ms} ms
-rllts has performed ${rll_ms / rllts_ms} times faster than rll (small file)`)
+																													// Nested interpolation <_<
+console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
+Time taken (${lightb("read-last-lines-ts")}): ${lightb(String(rllts_ms))} ms
+rllts has performed ${bold(String(rll_ms / rllts_ms))} times faster than rll ${dim("(small file, all lines)")}`)
 })
 
 test(`Speed difference on ${samples} loop samples | big file`, async () => {
@@ -38,9 +41,9 @@ test(`Speed difference on ${samples} loop samples | big file`, async () => {
 
 	expect(rllts_ms * 3).toBeLessThan(rll_ms)
 
-console.log(`Time taken    (read-last-lines): ${rll_ms} ms
-Time taken (read-last-lines-ts): ${rllts_ms} ms
-rllts has performed ${rll_ms / rllts_ms} times faster than rll (big file)`)
+console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
+Time taken (${lightb("read-last-lines-ts")}): ${lightb(String(rllts_ms))} ms
+rllts has performed ${bold(String(rll_ms / rllts_ms))} times faster than rll ${dim("(big file, few lines)")}`)
 })
 
 test(`Speed difference on ${samples / 10} loop big samples | big file`, async () => {
@@ -56,7 +59,7 @@ test(`Speed difference on ${samples / 10} loop big samples | big file`, async ()
 
 	expect(rllts_ms * 3).toBeLessThan(rll_ms)
 
-console.log(`Time taken    (read-last-lines): ${rll_ms} ms
-Time taken (read-last-lines-ts): ${rllts_ms} ms
-rllts has performed ${rll_ms / rllts_ms} times faster than rll (big file)`)
+console.log(`Time taken    (${orange("read-last-lines")}): ${orange(`${rll_ms} ms`)}
+Time taken (${lightb("read-last-lines-ts")}): ${lightb(String(rllts_ms))} ms
+rllts has performed ${bold(String(rll_ms / rllts_ms))} times faster than rll ${dim("(big file, all lines)")}`)
 })
