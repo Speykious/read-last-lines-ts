1d0f5e8e006f632e7de4b203ed61653cfe0a999a
Speykious <speykious.the.king@gmail.com>
2020-08-16 15:22:20 +0200

    Finally handling relative paths! I think it wasn't too complicated

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,29 @@
 import * as fs from "fs"
+import { resolve } from "path"
 
+/** Gets the path to the directory of the caller on a given layer.
+ * @param layer The layer where the caller is on the stack. */
+function getCallerDirPath(layer: number) {
+	const stackl = new Error().stack.split('\n')[layer]
+	return stackl.slice(
+		stackl.indexOf('/'),
+		stackl.lastIndexOf('/')+1
+	)
+}
+
+/** Handles the absolute/relative path
+ * @param layer The layer of stack call to pick from
+ * @param path The absolute/relative path to handle */
+function handlePath(layer: number, path: string) {
+	if (path.length && path[0] === "/")
+		return path
+	// The '+ 1' is here to make the layer relative to the
+	// handlePath call and not the getCallerDirPath call
+	resolve(getCallerDirPath(layer + 1), path)
+}
+
+/** Reads the previous character of a file, if the
+ * function's name doesn't already explain enough */
 function readPreviousChar(fd: number, stats: fs.Stats, ccc: number) {
 	const buffer = Buffer.alloc(1)
 	fs.readSync(fd, buffer, 0, 1, stats.size - 1 - ccc)
@@ -8,20 +32,20 @@ function readPreviousChar(fd: number, stats: fs.Stats, ccc: number) {
 
 /**
  * Reads the last lines of a file.
- * @param filepath Absolute path to the file.
+ * @param fp Absolute path to the file.
  * @param nlines Number of maximum lines to read.
  * @returns A buffer containing the lines that have been read.
  */
-export function readLastLines(
-	filepath: string,
-	nlines: number
-) {
-	if (!fs.existsSync(filepath))
-		throw new Error(`File '${filepath}' doesn't exist :(`)
+export function readLastLines(filepath: string, nlines: number) {
+	// 1 layer above to make it relative to the readLastLines call
+	const fp = handlePath(1, filepath)
+
+	if (!fs.existsSync(fp))
+		throw new Error(`File '${fp}' doesn't exist :(`)
 	
 	// Open the file before doing anything else
-	const fd = fs.openSync(filepath, "r")
-	const fstats = fs.statSync(filepath)
+	const fd = fs.openSync(fp, "r")
+	const fstats = fs.statSync(fp)
 	
 	let ichars = 0
 	let ilines = 0
